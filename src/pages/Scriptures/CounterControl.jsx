68ab0d7a1799501dcb6e1ea4a91c3ab040a8157f
wujiaan2022 <emmawu2004@gmail.com>
2025-04-06 23:22:41 +0200

    made the counter button stiky to bottom

--- src/pages/Scriptures/CounterControl.jsx
+++ src/pages/Scriptures/CounterControl.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function CounterControl({ count, increase, decrease, reset }) {
   return (
-    <div className="w-full flex items-center justify-between bg-gray-100 rounded-full px-8 py-2 shadow-inner">
+    <div className="sticky bottom-0 w-full flex items-center justify-between bg-gray-100 px-8 py-1 shadow-inner">
       <button
         onClick={reset}
         className="text-sm font-semibold text-red-600 hover:underline"
